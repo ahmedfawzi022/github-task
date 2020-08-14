ad26c606370741f7b5026e4c6f3b84d9d588785d
a.fawzy <a.fawzy@RD-AGABALLAH-Z>
2020-08-14 20:45:22 +0200

    deploy

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/github-task'));
+app.use(express.static(__dirname + '/dist/task-github'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/github-task/index.html'));});
+'/dist/task-github/index.html'));});
 app.listen(process.env.PORT || 8080);
